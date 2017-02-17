0b4704e5045e90c3404a134d2e362d82d0e74898
rajeevkalra <rajeev@kalra.in>
2017-02-17 17:31:39 +0530

    Update and rename d856-vendor.mk to d858-vendor.mk

--- d858-vendor.mk
+++ d858-vendor.mk
@@ -21,4 +21,4 @@ PRODUCT_PACKAGES += \
 		com.broadcom.bt \
 		com.broadcom.fm
 
-$(call inherit-product, vendor/lge/d856/d856-vendor-blobs.mk)
+$(call inherit-product, vendor/lge/d858/d858-vendor-blobs.mk)
